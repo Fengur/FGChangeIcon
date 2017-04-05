aff96fc818aa18e72aadb8f6402ccc25f16d0034
Fengur <duraedream@gmail.com>
2017-04-05 15:21:53 +0800

    听着七友提代码

--- FGChangeIcon/FGChangeIcon/ViewController.m
+++ FGChangeIcon/FGChangeIcon/ViewController.m
@@ -26,13 +26,44 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     UIWindow *window = app.window;
-    
     [UIView animateWithDuration:1.0f animations:^{
         _containLabel.alpha = 0;
         window.transform = CGAffineTransformMakeScale(1, 0.0005);
     } completion:^(BOOL finished) {
-        exit(0);
+        [self changeIcon];
+        window.transform = CGAffineTransformMakeScale(0.0005, 0.0005);
+        _containLabel.alpha = 1;
+        [UIView animateWithDuration:0.5 animations:^{
+            window.transform = CGAffineTransformMakeScale(1, 1);
+        }];
     }];
+    
+}
+
+
+- (void)changeIcon{
+    if ([UIApplication sharedApplication].supportsAlternateIcons) {
+        NSLog(@"this app can change app icon");
+    }else{
+        NSLog(@"sorry,this app can not change app icon");
+        return;
+    }
+    NSString *iconName = [[UIApplication sharedApplication] alternateIconName];
+    if (iconName) {
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }
+            NSLog(@"current icon's name -> %@",iconName);
+        }];
+    }else{
+        [[UIApplication sharedApplication] setAlternateIconName:@"icontwo" completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }
+            NSLog(@"current icon's name -> %@",iconName);
+        }];
+    }
 }
 
 
